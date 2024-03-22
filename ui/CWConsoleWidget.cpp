06bf342915c3affb70c23d7a97648a08f0a28949
Ladislav Foldyna <foldyna@gmail.com>
2024-03-22 15:58:39 +0100

    CW Console: Added CW Button tooltips with defined macro text

--- ui/CWConsoleWidget.cpp
+++ ui/CWConsoleWidget.cpp
@@ -89,18 +89,25 @@ void CWConsoleWidget::cwShortcutProfileComboChanged(QString profileName)
     const CWShortcutProfile &profile = shortcutManager->getCurProfile1();
 
     ui->macroButton1->setText("F1\n" + profile.shortDescs[0]);
+    ui->macroButton1->setToolTip(profile.macros[0]);
     ui->macroButton1->setShortcut(Qt::Key_F1);
     ui->macroButton2->setText("F2\n" + profile.shortDescs[1]);
+    ui->macroButton2->setToolTip(profile.macros[1]);
     ui->macroButton2->setShortcut(Qt::Key_F2);
     ui->macroButton3->setText("F3\n" + profile.shortDescs[2]);
+    ui->macroButton3->setToolTip(profile.macros[2]);
     ui->macroButton3->setShortcut(Qt::Key_F3);
     ui->macroButton4->setText("F4\n" + profile.shortDescs[3]);
+    ui->macroButton4->setToolTip(profile.macros[3]);
     ui->macroButton4->setShortcut(Qt::Key_F4);
     ui->macroButton5->setText("F5\n" + profile.shortDescs[4]);
+    ui->macroButton5->setToolTip(profile.macros[4]);
     ui->macroButton5->setShortcut(Qt::Key_F5);
     ui->macroButton6->setText("F6\n" + profile.shortDescs[5]);
+    ui->macroButton6->setToolTip(profile.macros[5]);
     ui->macroButton6->setShortcut(Qt::Key_F6);
     ui->macroButton7->setText("F7\n" + profile.shortDescs[6]);
+    ui->macroButton7->setToolTip(profile.macros[6]);
     ui->macroButton7->setShortcut(Qt::Key_F7);
 
     emit cwShortcutProfileChanged();
