d004d68300a35994f27819799e6c3d2137ff6c90
Ladislav Foldyna <foldyna@gmail.com>
2024-12-04 12:24:59 +0100

    Settings: disabled mode name modification

--- ui/SettingsDialog.cpp
+++ ui/SettingsDialog.cpp
@@ -180,6 +180,7 @@ SettingsDialog::SettingsDialog(MainWindow *parent) :
     ui->modeTableView->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
     ui->modeTableView->hideColumn(0);
     ui->modeTableView->hideColumn(2);
+    ui->modeTableView->setItemDelegateForColumn(1, new ReadOnlyDelegate());
     ui->modeTableView->setItemDelegateForColumn(4,new ComboFormatDelegate(QStringList()<<"CW"<< "PHONE" << "DIGITAL"));
     ui->modeTableView->setItemDelegateForColumn(5,new CheckBoxDelegate(ui->modeTableView));
     modeTableModel->select();
