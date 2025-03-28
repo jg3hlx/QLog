a8a31502222f888f3aebfebde497d06aa6859a90
Ladislav Foldyna <foldyna@gmail.com>
2025-03-28 15:17:52 +0100

    QSO Detail: Added editable QSLMSG_INTL and QSLMSG_RCVD

--- ui/QSODetailDialog.cpp
+++ ui/QSODetailDialog.cpp
@@ -375,6 +375,7 @@ QSODetailDialog::QSODetailDialog(const QSqlRecord &qso,
     mapper->addMapping(ui->qslLotwReceiveStatusLabel, LogbookModel::COLUMN_LOTW_RCVD);
     mapper->addMapping(ui->qslLotwSentStatusBox, LogbookModel::COLUMN_LOTW_SENT);
     mapper->addMapping(ui->qslReceivedMsgEdit, LogbookModel::COLUMN_QSLMSG_RCVD, "text");
+    mapper->addMapping(ui->qslSentMsgEdit, LogbookModel::COLUMN_QSLMSG_INTL, "text");
     mapper->addMapping(ui->qslSentViaBox, LogbookModel::COLUMN_QSL_SENT_VIA);
     mapper->addMapping(ui->qslViaEdit, LogbookModel::COLUMN_QSL_VIA);
     mapper->addMapping(ui->qslPaperReceiveDateEdit, LogbookModel::COLUMN_QSL_RCVD_DATE);
@@ -1838,7 +1839,6 @@ void QSOEditMapperDelegate::setModelData(QWidget *editor,
               || editor->objectName() == "qslEqslReceiveStatusLabel"
               || editor->objectName() == "qslLotwReceiveDateLabel"
               || editor->objectName() == "qslLotwReceiveStatusLabel"
-              || editor->objectName() == "qslReceivedMsgEdit"
             )
     {
         /* do not save */
